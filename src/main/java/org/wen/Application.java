19542df327bc6b41f7fbd953174d0b9db5a8ff9c
lovewen <lovewen@qq.com>
2022-01-02 21:28:57 +0800

    测试提交

--- src/main/java/org/wen/Application.java
+++ src/main/java/org/wen/Application.java
@@ -13,5 +13,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
+        System.out.println("lucky is run ❤");
     }
 }
