f56f7203f8dd385aaf78eaf15e9c0e225d5067d2
lovewen <lovewen@qq.com>
2022-01-02 21:12:29 +0800

    初创lucky脚手架

---
+++ src/main/java/org/wen/Application.java
@@ -0,0 +1,17 @@
+package org.wen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author loveweni
+ * @organization coder
+ * @create 2022-01-02 21:01
+ * @description Application启动类
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
