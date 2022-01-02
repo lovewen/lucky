a6acd2f756f93fd7fb4be1c325f7b42d2706ba09
lovewen <lovewen@qq.com>
2022-01-02 21:43:14 +0800

    测试提交

--- src/main/java/org/wen/Application.java
+++ src/main/java/org/wen/Application.java
@@ -14,6 +14,5 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
         System.out.println("lucky is run ❤");
-        System.out.println("lucky is run ❤");
     }
 }
