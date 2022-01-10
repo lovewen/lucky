4c33ca67ff2ad5fba9263a55626338a00ed88df0
吴承硕 <wuchengshuo@qq.com>
2022-01-10 14:04:23 +0800

    封装实体类代码

--- src/main/java/org/wen/business/Business.java
+++ src/main/java/org/wen/business/Business.java
@@ -9,23 +9,27 @@ import org.wen.data.service.impl.OcProductDescriptionServiceImpl;
 import org.wen.data.service.impl.OcProductImageServiceImpl;
 import org.wen.data.service.impl.OcProductServiceImpl;
 import org.wen.tools.HttpTool;
+import sun.security.provider.certpath.OCSP;
 
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
  * @author loveweni
  * @company: 顼龙科技
  * @date 2022/01/09 10:31:53
- * @description: description
+ * @description: 执行商品插入数据库
  */
 public class Business {
 
     @Autowired
-    private OcProductServiceImpl productService;
+    private OcProductServiceImpl productService = new OcProductServiceImpl();
     @Autowired
-    private OcProductDescriptionServiceImpl descriptionService;
+    private OcProductDescriptionServiceImpl descriptionService = new OcProductDescriptionServiceImpl();
     @Autowired
-    private OcProductImageServiceImpl imageService;
+    private OcProductImageServiceImpl imageService = new OcProductImageServiceImpl();
 
     /**
      * 声明实体类信息
@@ -34,15 +38,66 @@ public class Business {
     OcProductDescription ocProductDescription;
     OcProductImage ocProductImage;
 
-
-
     public String execute() throws Exception{
         //1. 获取并解析参数
-        JSONObject param = new HttpTool().getProduct();
+        //JSONObject param = new HttpTool().getProduct();
         //2. 封装product实体类和description实体类和image实体类
         ocProduct = new OcProduct();
         ocProductDescription = new OcProductDescription();
         ocProductImage = new OcProductImage();
+
+        /**
+         * 对商品表的封装
+         */
+        ocProduct.setModel("商品名称");//商品名称
+        ocProduct.setSku("17");//库存
+        ocProduct.setUpc("1");
+        ocProduct.setEan("1");
+        ocProduct.setJan("1");
+        ocProduct.setIsbn("1");
+        ocProduct.setMpn("1");
+        ocProduct.setLocation("1");
+        ocProduct.setQuantity(new Integer("15"));
+        ocProduct.setStockStatusId(1L);
+        ocProduct.setImage("https://img2.baidu.com/it/u=1970005619,2283568274&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=779");//商品图片
+        ocProduct.setManufacturerId(1L);
+        ocProduct.setShipping(new Integer("15"));
+        ocProduct.setPrice(new BigDecimal("15.22"));
+        ocProduct.setPoints(new Integer("15"));
+        ocProduct.setTaxClassId(1L);
+        ocProduct.setDateAvailable(new Date());
+        ocProduct.setWeight(new BigDecimal("15.88"));
+        ocProduct.setWeightClassId(1L);
+        ocProduct.setLength(new BigDecimal("15.01"));
+        ocProduct.setWidth(new BigDecimal("15.21"));
+        ocProduct.setHeight(new BigDecimal("15.23"));
+        ocProduct.setLengthClassId(1L);
+        ocProduct.setSubtract(new Integer("1"));
+        ocProduct.setMinimum(1L);
+        ocProduct.setMinimum(1L);
+        ocProduct.setSortOrder(1L);
+        ocProduct.setStatus(new Integer("1"));
+        ocProduct.setViewed(new Integer("1"));
+        ocProduct.setDateAdded(new Date());
+        ocProduct.setDateModified(new Date());
+
+        /**
+         * 对商品描述的封装
+         */
+        ocProductDescription.setName("哇哈哈");
+        ocProductDescription.setDescription("测试商品数据");
+        ocProductDescription.setTag("1");
+        ocProductDescription.setMetaTitle("1");
+        ocProductDescription.setMetaDescription("1");
+        ocProductDescription.setMetaKeyword("1");
+
+        /**
+         * 对商品图片的封装
+         */
+        ocProductImage.setImage("https://img2.baidu.com/it/u=1970005619,2283568274&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=779");
+        ocProductImage.setSortOrder(new Integer("1"));
+
+
 
         //3. 插入product表
         int i = productService.insertOcProduct(ocProduct);
